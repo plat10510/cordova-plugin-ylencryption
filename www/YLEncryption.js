f14706dd3fb76f6714a4da64f9f962f86506f736
ka <plat10510@163.com>
2018-10-26 18:06:08 +0800

    改吧改吧

--- www/YLEncryption.js
+++ www/YLEncryption.js
@@ -1,26 +1,10 @@
 var exec = require('cordova/exec');
 
-function YLEncryption(){
-    
-}
 
-YLEncryption.prototype.encrypt = function(options,successCallback,errorCallback){
-    exec(successCallback, errorCallback, 'YLEncryption', 'encrypt', [options]);
-}
+exports.encrypt = function (parms, success, error) {
+    exec(success, error, 'YLEncryption', 'encrypt', [parms]);
+};
 
-YLEncryption.prototype.decrypt = function(options,successCallback,errorCallback){
-    exec(successCallback, errorCallback, 'YLEncryption', 'decrypt', [options]);
-}
-
-
-YLEncryption.install = function(){
-    if(!window.plugins){
-        window.plugins = {};
-    }
-
-    window.plugins.ylencryption = new YLEncryption();
-
-    return window.plugins.ylencryption;
-}
-
-cordova.addConstructor(YLEncryption.install);
+exports.decrypt = function (parms, success, error) {
+    exec(success, error, 'YLEncryption', 'decrypt', [parms]);
+};
